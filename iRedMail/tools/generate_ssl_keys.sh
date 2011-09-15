d69f747fcdf31af670befe54a4121fe264cd1567
Zhang Huangbin <zhb@iredmail.org>
2011-09-15 13:53:21 +0800

    Better comment in tools/generate_ssl_keys.sh.

--- iRedMail/tools/generate_ssl_keys.sh
+++ iRedMail/tools/generate_ssl_keys.sh
@@ -4,19 +4,63 @@
 # Purpose:  Create new SSL keys.
 # Project:  iRedMail (http://www.iredmail.org/)
 
-export HOSTNAME="$(hostname -f)"
-export ROOTDIR="$(pwd)"
+# USAGE:
+# 1) Edit variables which starts with TLS_ below, then save file.
+# 2) Execute shell command:
+#
+#       # bash generate_ssl_keys.sh
+#
+#    It will create two new files under CURRENT directory:
+#
+#       - certs/iRedMail_CA.pem: Used to replace file on iRedMail server:
+#           + on RHEL/CentOS/Scientific Linux: /etc/pki/tls/certs/iRedMail_CA.pem
+#           + on Debian/Ubuntu/openSUSE: /etc/ssl/certs/iRedMail_CA.pem
+#           + on FreeBSD: /etc/ssl/certs/iRedMail_CA.pem
+#       - private/iRedMail.key: Used to replace file on iRedMail server:
+#           + on RHEL/CentOS/Scientific Linux: /etc/pki/tls/private/iRedMail.key
+#           + on Debian/Ubuntu/openSUSE: /etc/ssl/private/iRedMail.key
+#           + on FreeBSD: /etc/ssl/private/iRedMail.key
+#
+# 3) Grant read access to all users. e.g. on RHEL/CentOS/Scientific Linux:
+#
+#   # chmod +r /etc/ssl/certs/iRedMail_CA.pem
+#   # chmod +r /etc/ssl/private/iRedMail.key
+#
+#   If you need more restrict file permission, please use file system ACL instead.
+#   Refer to command 'setfacl' and 'getfacl' for more detail.
+#
+# 4) Restart all services which provides SSL secure connection. e.g. http,
+#    dovecot, postfix, etc. A system reboot should be easier if possible.
+#
 
-# SSL key.
+export HOSTNAME="$(hostname -f)"
+
+# SSL key related settings.
+# Country.
+export TLS_COUNTRY='CN'
+
+# State.
+export TLS_STATE='GuangDong'
+
+# City.
+export TLS_CITY='ShenZhen'
+
+# Company name here, e.g. Apple Inc.
+export TLS_COMPANY="${HOSTNAME}"
+
+# Department name.
+export TLS_DEPARTMENT='IT'
+
+# Hostname of your mail server.
+export TLS_HOSTNAME="${HOSTNAME}"
+
+# Server admininistrator's email address.
+export TLS_ADMIN="root@${HOSTNAME}"
+
+# Do not edit below lines.
+export ROOTDIR="$(pwd)"
 export SSL_CERT_FILE="${ROOTDIR}/certs/iRedMail_CA.pem"
 export SSL_KEY_FILE="${ROOTDIR}/private/iRedMail.key"
-export TLS_COUNTRY='CN'
-export TLS_STATE='GuangDong'
-export TLS_CITY='ShenZhen'
-export TLS_COMPANY="${HOSTNAME}"
-export TLS_DEPARTMENT='IT'
-export TLS_HOSTNAME="${HOSTNAME}"
-export TLS_ADMIN="root@${HOSTNAME}"
 
 # Create SSL certs/private files.
 gen_pem_key()
