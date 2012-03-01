d9797b103ad28fe4b7c6e28b2d4d3752b824d06c
Zhang Huangbin <zhb@iredmail.org>
2012-03-01 19:15:09 +0800

    Don't enable auth_default_realm in Dovecot by default.

--- iRedMail/samples/used_quota.sql
+++ iRedMail/samples/used_quota.sql
@@ -1,10 +1,10 @@
-#
-# Table `used_quota`. Used for Dovecot to store realtime quota.
-#
-# WARNING:
-#   - Works only with Dovecot 1.2+.
-#   - This table is not required in MySQL backend.
-#
+--
+-- Table `used_quota`. Used for Dovecot to store realtime quota.
+--
+-- WARNING:
+--   - Works only with Dovecot 1.2+.
+--   - This table is not required in MySQL backend.
+--
 CREATE TABLE IF NOT EXISTS `used_quota` (
     `username` VARCHAR(255) NOT NULL,
     `bytes` BIGINT NOT NULL DEFAULT 0,
