b010c18edf2c647377469498aee3842d54f9ef7e
Zhang Huangbin <zhb@iredmail.org>
2013-08-16 20:31:05 +0800

    Code cleanup.

--- iRedMail/functions/apache_php.sh
+++ iRedMail/functions/apache_php.sh
@@ -302,14 +302,14 @@ EOF
     perl -pi -e 's#^(disable_functions.*=)(.*)#${1}$ENV{PHP_DISABLED_FUNCTIONS}; ${2}#' ${PHP_INI}
 
     ECHO_DEBUG "Hide PHP Version in Apache from remote users requests: ${PHP_INI}."
-    perl -pi -e 's#^(expose_php.*=)#${1} Off;#' ${PHP_INI}
+    perl -pi -e 's#^(expose_php.*=).*#${1} Off;#' ${PHP_INI}
 
     ECHO_DEBUG "Increase 'memory_limit' to 256M: ${PHP_INI}."
-    perl -pi -e 's#^(memory_limit = )#${1} 256M;#' ${PHP_INI}
+    perl -pi -e 's#^(memory_limit = ).*#${1} 256M;#' ${PHP_INI}
 
     ECHO_DEBUG "Increase 'upload_max_filesize', 'post_max_size' to 10/12M: ${PHP_INI}."
     perl -pi -e 's/^(upload_max_filesize.*=).*/${1} 10M;/' ${PHP_INI}
-    perl -pi -e 's/^(post_max_size.*=)/${1} 12M;/' ${PHP_INI}
+    perl -pi -e 's/^(post_max_size.*=).*/${1} 12M;/' ${PHP_INI}
 
     ECHO_DEBUG "Disable php extension: suhosin. ${PHP_INI}."
     perl -pi -e 's/^(suhosin.session.encrypt.*=)/${1} Off;/' ${PHP_INI}
