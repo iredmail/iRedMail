a45ee23491892b6544f9301d11cf366b5b58cb8b
Zhang Huangbin <zhb@iredmail.org>
2014-07-03 22:10:29 +0800

    Fixed: incorrect quota dict name in dovecot quota warning script.

--- iRedMail/samples/dovecot/dovecot2-quota-warning.sh
+++ iRedMail/samples/dovecot/dovecot2-quota-warning.sh
@@ -6,8 +6,8 @@ PERCENT=${1}
 USER=${2}
 
 # Use "plugin/quota=maildir:User quota:noenforcing" for maildir quota.
-cat << EOF | PH_DOVECOT_DELIVER -d ${USER} -o "plugin/quota=dict:User quota::noenforcing:proxy::quota"
-From: no-reply@PH_HOSTNAME
+cat << EOF | PH_DOVECOT_DELIVER -d ${USER} -o "plugin/quota=dict:User quota::noenforcing:proxy::quotadict"
+From: no-reply@$(hostname -f)
 Subject: Warning: Your mailbox is now ${PERCENT}% full.
 
 Your mailbox is now ${PERCENT}% full, please clean up some mails for further incoming mails.
@@ -16,8 +16,8 @@ EOF
 # Send a copy to postmaster@ if mailbox is greater than or equal to 95% full.
 if [ ${PERCENT} -ge 95 ]; then
     DOMAIN="$(echo ${USER} | awk -F'@' '{print $2}')"
-    cat << EOF | PH_DOVECOT_DELIVER -d postmaster@${DOMAIN} -o "plugin/quota=dict:User quota::noenforcing:proxy::quota"
-From: no-reply@PH_HOSTNAME
+    cat << EOF | PH_DOVECOT_DELIVER -d postmaster@${DOMAIN} -o "plugin/quota=dict:User quota::noenforcing:proxy::quotadict"
+From: no-reply@$(hostname -f)
 Subject: Mailbox Quota Warning: ${PERCENT}% full, ${USER}
 
 Your mailbox is now ${PERCENT}% full, please clean up some mails for
