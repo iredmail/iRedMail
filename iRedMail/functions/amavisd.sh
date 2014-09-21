7e2669891f193a55f05274eed387c9a8fa44ff38
Zhang Huangbin <zhb@iredmail.org>
2014-09-22 06:19:06 +0800

    RHEL/CentOS 7: Don't open networks ports for ldap/ldaps service in firewalld.

--- iRedMail/functions/amavisd.sh
+++ iRedMail/functions/amavisd.sh
@@ -519,9 +519,12 @@ EOF
     fi
 
     cat >> ${AMAVISD_CONF} <<EOF
+# Reporting and quarantining.
 @storage_sql_dsn = (
     ['DBI:${perl_dbi_type}:database=${AMAVISD_DB_NAME};host=${SQL_SERVER};port=${SQL_SERVER_PORT}', '${AMAVISD_DB_USER}', '${AMAVISD_DB_PASSWD}'],
 );
+
+# Lookup for per-recipient, per-domain and global policy.
 @lookup_sql_dsn = @storage_sql_dsn;
 EOF
 
