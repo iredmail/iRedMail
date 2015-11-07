e0b56b4e87869ce0df28c36da1484b3146d675e2
Zhang Huangbin <zhb@iredmail.org>
2015-11-07 09:38:10 +0800

    Code cleanup in tools/find_top_sasl_usernames.sh.

--- iRedMail/tools/find_top_sasl_usernames.sh
+++ iRedMail/tools/find_top_sasl_usernames.sh
@@ -2,15 +2,25 @@
 # Purpose: Find usernames used for smtp authentication in Postfix log file,
 #          sorted by login times.
 
-MAIL_LOG="$1"
+MAILLOG_FILE="$1"
 
-if [ -z ${MAIL_LOG} ]; then
+# Detect mail log file if not specified on command line.
+if [ -z ${MAILLOG_FILE} ]; then
+    for f in /var/log/maillog /var/log/mail.log; do
+        if [ -f ${f} ]; then
+            MAILLOG_FILE="${f}"
+            break
+        fi
+    done
+fi
+
+if [ -z ${MAILLOG_FILE} ]; then
     echo "Please specify the mail log file: $0 /path/to/maillog"
     exit 255
 fi
 
 tmpfile="/tmp/sasl_username_${RANDOM}"
-grep 'sasl_username=' ${MAIL_LOG} > ${tmpfile}
+grep 'sasl_username=' ${MAILLOG_FILE} > ${tmpfile}
 awk '{print $NF}' ${tmpfile}  | sort | uniq -c | sort -n
 
 rm -f ${tmpfile}
