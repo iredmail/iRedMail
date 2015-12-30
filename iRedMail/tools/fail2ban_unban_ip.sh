6c59bd4974cdfeff7929b0b75642e104784ae599
Zhang Huangbin <zhb@iredmail.org>
2015-12-30 21:44:21 +0800

    PSlightly improve tools/fail2ban_unban_ip.sh: print help message if no argument on command line.

--- iRedMail/tools/fail2ban_unban_ip.sh
+++ iRedMail/tools/fail2ban_unban_ip.sh
@@ -7,6 +7,19 @@
 #
 #   bash fail2ban_unban_ip.sh 192.168.1.1 [192.168.2.2 ...]
 
+if [ X"$#" == X'0' ]; then
+    cat <<EOF
+Usage: specify the IP addresses you want to unban on command line. For example:
+
+$ bash fail2ban_unban_ip.sh 173.254.22.21
+
+Multiple IP addresses must be spearated by space.
+
+$ bash fail2ban_unban_ip.sh 173.254.22.21 8.8.8.8 8.8.4.4
+
+EOF
+fi
+
 # Get all IP addresses from command line.
 IPS="$@"
 
