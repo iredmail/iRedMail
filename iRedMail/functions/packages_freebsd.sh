ad3ec0808684afa436a5e09182198dd57748d9fe
Zhang Huangbin <zhb@iredmail.org>
2016-01-22 10:55:57 +0800

    * Run /opt/iredapd/tools/spf_to_greylist_whitelists.py every 10 minutes. * FreeBSD: fix incorrect port name for sogo (www/sogo-activesync is the one we want).

--- iRedMail/functions/packages_freebsd.sh
+++ iRedMail/functions/packages_freebsd.sh
@@ -1013,7 +1013,7 @@ EOF
 
     # SOGo groupware.
     if [ X"${USE_SOGO}" == X'YES' ]; then
-        ALL_PORTS="${ALL_PORTS} devel/sope www/sogo www/sogo-activesync"
+        ALL_PORTS="${ALL_PORTS} devel/sope www/sogo-activesync"
 
         if [ X"${BACKEND}" == X'OPENLDAP' -o X"${BACKEND}" == X'MYSQL' ]; then
             ${CMD_SED} -e 's#OPTIONS_FILE_UNSET+=MYSQL#OPTIONS_FILE_SET+=MYSQL#' /var/db/ports/devel_sope/options
