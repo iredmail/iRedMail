ef0ad2b1d3afdb23d17212cd1d9e7149c10e4299
Zhang Huangbin <zhb@iredmail.org>
2017-01-11 11:44:43 +0800

    Revert last commit: create Dovecot log files manually - with correct file owner/group. Fix incorrect log string generated by dovecot in rsyslog config file. Monitor /var/log/dovecot.log in Fail2ban, it contains managesieve log.

--- iRedMail/functions/dovecot.sh
+++ iRedMail/functions/dovecot.sh
@@ -375,8 +375,11 @@ dovecot_log() {
         # Although no need to create log files manually, but fail2ban will skip
         # the log file which doesn't exist while fail2ban starts up, so we
         # create it manually to avoid this.
-        ECHO_DEBUG "Restart rsyslog service to load new config file."
-        service_control restart rsyslog
+        for f in ${DOVECOT_LOG_FILE} ${DOVECOT_SYSLOG_FILE_LDA} ${DOVECOT_SYSLOG_FILE_IMAP} ${DOVECOT_SYSLOG_FILE_POP3}; do
+            ECHO_DEBUG "Create dovecot log file: ${f}."
+            touch ${f}
+            chown ${SYSLOG_DAEMON_USER}:${SYSLOG_DAEMON_GROUP} ${f}
+        done
     else
         # Disable syslog and use Dovecot internal log system
         perl -pi -e 's/^(syslog_facility.*)/#${1}/' ${DOVECOT_CONF}
