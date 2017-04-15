6b865eaac4135d3aca49786b0283efc329b96f70
Zhang Huangbin <zhb@iredmail.org>
2017-04-15 19:18:47 +0800

    Install lrzip on debian/ubuntu. Better sql command to handle amavisd policy relationship.

--- iRedMail/samples/amavisd/bypass.sql
+++ iRedMail/samples/amavisd/bypass.sql
@@ -1,9 +1,9 @@
 INSERT INTO policy (policy_name,
-                    spam_lover, banned_files_lover, bad_header_lover,
-                    spam_quarantine_to, banned_quarantine_to, bad_header_quarantine_to)
+                    spam_lover, virus_lover, banned_files_lover, bad_header_lover,
+                    spam_quarantine_to, virus_quarantine_to, banned_quarantine_to, bad_header_quarantine_to)
             VALUES ('@.',
-                    'Y', 'Y', 'Y',
-                    '', '', '');
+                    'Y', 'N', 'Y', 'Y',
+                    '', 'virus-quarantine', '', '');
 
 INSERT INTO users (priority, email) VALUES (0, '@.');
 
