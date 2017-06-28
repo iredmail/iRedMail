8aa9ff7fac236b561b8e05acc80eadd062bf78a0
Zhang Huangbin <zhb@iredmail.org>
2017-06-28 17:36:20 +0800

    Enable three columns in Roundcube by default.

--- iRedMail/samples/roundcubemail/config.inc.php
+++ iRedMail/samples/roundcubemail/config.inc.php
@@ -62,6 +62,7 @@ $config['autoexpand_threads'] = 2;
 $config['check_all_folders'] = true;
 $config['default_font_size'] = '12pt';
 $config['message_show_email'] = true;
+$config['layout'] = 'widescreen';   // three columns
 //$config['skip_deleted'] = true;
 
 // PLUGINS
