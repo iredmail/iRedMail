a612e07bac7478a362f021553fe1a08c554623bb
Zhang Huangbin <zhb@iredmail.org>
2018-02-15 02:25:44 +0800

    Update parameter 'enigma_pgp_homedir' for Roundcube plugin 'enigma'. Nginx: Cache image/css/javascript/pdf files for 1 day by default.

--- iRedMail/functions/nginx.sh
+++ iRedMail/functions/nginx.sh
@@ -54,7 +54,8 @@ nginx_config()
     #
     # Enable modular config files
     #
-    _modular_conf='client_max_body_size.conf \
+    _modular_conf='cache.conf \
+        client_max_body_size.conf \
         default_type.conf \
         gzip.conf \
         log.conf \
