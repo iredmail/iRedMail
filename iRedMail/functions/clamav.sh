aa3ee9c00e672ee386ebb0228664e04036c35257
Zhang Huangbin <zhb@iredmail.org>
2018-06-17 11:13:58 +0200

    Fixed: don't set AllowSupplementaryGroups=true in clamav for Debian 9 and Ubuntu 18.04.

--- iRedMail/functions/clamav.sh
+++ iRedMail/functions/clamav.sh
@@ -57,7 +57,9 @@ clamav_config()
 
     # Enable AllowSupplementaryGroups
     perl -pi -e 's/^(AllowSupplementaryGroups.*)/#${1}/' ${CLAMD_CONF}
-    if [ X"${DISTRO_CODENAME}" != X'stretch' -o X"${DISTRO_CODENAME}" != X'bionic' ]; then
+    if [ X"${DISTRO_CODENAME}" == X'stretch' -o X"${DISTRO_CODENAME}" == X'bionic' ]; then
+        :
+    else
         echo 'AllowSupplementaryGroups true' >> ${CLAMD_CONF}
     fi
 
