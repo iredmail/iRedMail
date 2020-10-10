9a7cc6134920d1a794617adc3404c2e4c6917317
Zhang Huangbin <zhb@iredmail.org>
2020-10-10 12:47:21 +0800

    Update shasum of repacked iRedAdmin OSE. Remove unused update/ldap/updateLDAPValues_*.py. Our upgrade tutorials stick to files in iRedMail-1.0 release, so removing them now won't impact upgrading old iRedMail releases.

--- update/ldap/update-ldap-dovecot-2.3.py
+++ update/ldap/update-ldap-dovecot-2.3.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Author:   Zhang Huangbin <zhb _at_ iredmail.org>
 # Purpose:  Add missing attribute/value pairs required by Dovecot-2.3.
 # Date:     Apr 12, 2018.
@@ -13,19 +13,19 @@ bind_dn = 'cn=Manager,dc=example,dc=com'
 bind_pw = 'password'
 
 # Initialize LDAP connection.
-print "* Connecting to LDAP server: %s" % uri
+print("* Connecting to LDAP server: {}".format(uri))
 conn = ldap.initialize(uri=uri, trace_level=0,)
 conn.bind_s(bind_dn, bind_pw)
 
 # Get all mail users.
-print "* Get mail accounts ..."
+print("* Get mail accounts ...")
 allUsers = conn.search_s(basedn,
                          ldap.SCOPE_SUBTREE,
                          "(&(objectClass=mailUser)(|(enabledService=imapsecured)(enabledService=pop3secured)(enabledService=smtpsecured)))",
                          ['mail', 'enabledService'])
 
 total = len(allUsers)
-print "* Updating %d user(s)." % (total)
+print("* Updating {} user(s).".format(total))
 
 # Counter.
 count = 1
@@ -51,19 +51,19 @@ for (dn, entry) in allUsers:
         _update = True
 
     if _update:
-        print "* (%d of %d) Updating user: %s" % (count, total, mail)
+        print("* ({} of {}) Updating user: {}".format(count, total, mail))
         mod_attr = [(ldap.MOD_REPLACE, 'enabledService', enabledService)]
         try:
             conn.modify_s(dn, mod_attr)
-        except Exception, e:
-            print "Error while updating user:", mail, e
+        except Exception as e:
+            print("Error while updating user {}: {}".format(mail, repr(e)))
     else:
-        print "* [SKIP] No update required for user:", mail
+        print("* [SKIP] No update required for user: {}".format(mail))
 
     count += 1
 
 # Unbind connection.
-print "* Unbind LDAP server."
+print("* Unbind LDAP server.")
 conn.unbind()
 
-print "* Update completed."
+print("* Update completed.")
