6f239e675d69fe8169cde96dcda4c24e2e0e53e9
tenhishadow <adm@tenhi.dev>
2021-02-20 19:30:41 +0300

    refactfor spamassassin.sh

--- functions/spamassassin.sh
+++ functions/spamassassin.sh
@@ -9,41 +9,46 @@ sa_config()
 {
     ECHO_INFO "Configure SpamAssassin (content-based spam filter)."
 
-    backup_file ${SA_LOCAL_CF}
+    backup_file "${SA_LOCAL_CF}"
 
     ECHO_DEBUG "Copy sample SpamAssassin config file: ${SAMPLE_DIR}/spamassassin/local.cf -> ${SA_LOCAL_CF}."
-    cp -f ${SAMPLE_DIR}/spamassassin/local.cf ${SA_LOCAL_CF}
-    cp -f ${SAMPLE_DIR}/spamassassin/razor.conf ${SA_PLUGIN_RAZOR_CONF}
+    cp -f "${SAMPLE_DIR}"/spamassassin/local.cf "${SA_LOCAL_CF}"
+    cp -f "${SAMPLE_DIR}"/spamassassin/razor.conf "${SA_PLUGIN_RAZOR_CONF}"
 
-    perl -pi -e 's#PH_SA_PLUGIN_RAZOR_CONF#$ENV{SA_PLUGIN_RAZOR_CONF}#g' ${SA_LOCAL_CF}
+    perl -pi -e 's#PH_SA_PLUGIN_RAZOR_CONF#$ENV{SA_PLUGIN_RAZOR_CONF}#g' "${SA_LOCAL_CF}"
 
     ECHO_DEBUG "Enable crontabs for SpamAssassin update."
-    if [ X"${DISTRO}" == X'RHEL' ]; then
-        if [ -f ${ETC_SYSCONFIG_DIR}/sa-update ]; then
-            perl -pi -e 's/^#(SAUPDATE=yes)/${1}/' ${ETC_SYSCONFIG_DIR}/sa-update
-        fi
+    case "${DISTRO}" in
+        "")
+            echo "ERROR: cannot define distro"
+            exit 255
+            ;;
+        RHEL)
+            [[ -f "${ETC_SYSCONFIG_DIR}"/sa-update ]] && \
+                perl -pi -e 's/^#(SAUPDATE=yes)/${1}/' "${ETC_SYSCONFIG_DIR}"/sa-update
+            # CentOS 7.
+            [[ -f /etc/cron.d/sa-update ]] && \
+                chmod 0644 /etc/cron.d/sa-update; \
+                perl -pi -e 's/#(10.*)/${1}/' /etc/cron.d/sa-update
+            ;;
+        UBUNTU|DEBIAN)
+            perl -pi -e 's#^(CRON=)0#${1}1#' /etc/default/spamassassin
+            ;;
+        FREEBSD)
+            ECHO_DEBUG "Compile SpamAssassin ruleset into native code."
+            sa-compile >> "${INSTALL_LOG}" 2>&1
+            ;;
+        *)
+            echo "ERROR: unsupported distro"
+            exit 255
+            ;;
+    esac
 
-        # CentOS 7.
-        if [ -f /etc/cron.d/sa-update ]; then
-            chmod 0644 /etc/cron.d/sa-update
-            perl -pi -e 's/#(10.*)/${1}/' /etc/cron.d/sa-update
-        fi
-    elif [ X"${DISTRO}" == X'UBUNTU' -o X"${DISTRO}" == X'DEBIAN' ]; then
-        perl -pi -e 's#^(CRON=)0#${1}1#' /etc/default/spamassassin
-    fi
+    printf '%s\n' \
+        'SpamAssassin:' \
+        '    * Configuration files and rules:' \
+        "        - ${SA_CONF_DIR}" \
+        "        - ${SA_CONF_DIR}/local.cf" >> "${TIP_FILE}"
 
-    if [ X"${DISTRO}" == X'FREEBSD' ]; then
-        ECHO_DEBUG "Compile SpamAssassin ruleset into native code."
-        sa-compile >> ${INSTALL_LOG} 2>&1
-    fi
-
-    cat >> ${TIP_FILE} <<EOF
-SpamAssassin:
-    * Configuration files and rules:
-        - ${SA_CONF_DIR}
-        - ${SA_CONF_DIR}/local.cf
-
-EOF
-
-    echo 'export status_sa_config="DONE"' >> ${STATUS_FILE}
+    echo 'export status_sa_config="DONE"' >> "${STATUS_FILE}"
 }
