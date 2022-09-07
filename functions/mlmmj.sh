28dd5eb4290d5f277f8e5d2f6a30e4d317956632
Zhang Huangbin <zhb@iredmail.org>
2022-09-07 20:25:51 +0800

    Fix an incorrect regular expression while updating mlmmjadmin config file.

--- functions/mlmmj.sh
+++ functions/mlmmj.sh
@@ -189,7 +189,7 @@ EOF
     # Copy rc script file
     if [ X"${USE_SYSTEMD}" == X'YES' ]; then
         if [ X"${DISTRO}" == X'RHEL' ]; then
-            perl -pi -e 's/^(plugins.*)python,(.*)/${1}$ENV{UWSGI_PLUGIN_PYTHON}${2}/g' ${MLMMJADMIN_ROOT_DIR_SYMBOL_LINK}/rc_scripts/uwsgi/rhel.ini
+            perl -pi -e 's/^(plugins.*)python,(.*)/${1}$ENV{UWSGI_PLUGIN_PYTHON},${2}/g' ${MLMMJADMIN_ROOT_DIR_SYMBOL_LINK}/rc_scripts/uwsgi/rhel.ini
 
             cp -f ${MLMMJADMIN_ROOT_DIR_SYMBOL_LINK}/rc_scripts/systemd/rhel.service ${SYSTEMD_SERVICE_DIR}/${MLMMJADMIN_RC_SCRIPT_NAME}.service >> ${INSTALL_LOG} 2>&1
             chmod 0644 ${SYSTEMD_SERVICE_DIR}/${MLMMJADMIN_RC_SCRIPT_NAME}.service
