75641d595cf2181e849c2e82e8423c578b02c02d
Zhang Huangbin <zhb@iredmail.org>
2024-08-19 18:01:44 +0800

    Fix incorrect sogo SQL table name.

--- functions/sogo.sh
+++ functions/sogo.sh
@@ -145,6 +145,7 @@ sogo_config() {
     perl -pi -e 's#PH_SOGO_DB_TABLE_STORE#$ENV{SOGO_DB_TABLE_STORE}#g' ${SOGO_CONF}
     perl -pi -e 's#PH_SOGO_DB_TABLE_ACL#$ENV{SOGO_DB_TABLE_ACL}#g' ${SOGO_CONF}
     perl -pi -e 's#PH_SOGO_DB_TABLE_CACHE_FOLDER#$ENV{SOGO_DB_TABLE_CACHE_FOLDER}#g' ${SOGO_CONF}
+    perl -pi -e 's#PH_SOGO_DB_TABLE_ADMIN#$ENV{SOGO_DB_TABLE_ADMIN}#g' ${SOGO_CONF}
 
     perl -pi -e 's#PH_SQL_SERVER_PORT#$ENV{SQL_SERVER_PORT}#g' ${SOGO_CONF}
     if [ X"${SQL_SERVER_ADDRESS_IS_IPV6}" == X'YES' ]; then
